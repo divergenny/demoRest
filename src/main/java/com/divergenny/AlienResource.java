733331f34a68f8bb16e15507cf61b21b4e32e957
divergenny <divergenny@gmail.com>
2022-03-03 10:45:12 +0500

    add: list as resource

--- src/main/java/com/divergenny/AlienResource.java
+++ src/main/java/com/divergenny/AlienResource.java
@@ -5,16 +5,25 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Path("aliens")
 public class AlienResource {
 
     @GET
     @Produces(MediaType.APPLICATION_XML)
-    public Alien getAlien() {
+    public List<Alien> getAlien() {
         System.out.println("getAlien called...");
         Alien a1 = new Alien();
         a1.setName("Navin");
         a1.setPoints(60);
-        return a1;
+
+        Alien a2 = new Alien();
+        a2.setName("Arati");
+        a2.setPoints(70);
+        List<Alien> alienList = Arrays.asList(a1, a2);
+
+        return alienList;
     }
 }

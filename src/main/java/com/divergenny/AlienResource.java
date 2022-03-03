1351b57b16c577d5994dd1aaf2d15cd6aad21346
divergenny <divergenny@gmail.com>
2022-03-03 10:21:06 +0500

    add: resource class

---
+++ src/main/java/com/divergenny/AlienResource.java
@@ -0,0 +1,20 @@
+package com.divergenny;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("aliens")
+public class AlienResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Alien getAlien() {
+        System.out.println("getAlien called...");
+        Alien a1 = new Alien();
+        a1.setName("Navin");
+        a1.setPoints(60);
+        return a1;
+    }
+}
